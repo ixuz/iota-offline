03e36e46a49041493f2853f93e47f7ec564b6ad4
Anton Wiklund <ixuz07@gmail.com>
2018-09-27 02:03:26 +0200

    Try again

--- src/ts/qr.ts
+++ src/ts/qr.ts
@@ -1,4 +1,5 @@
-import jsQR from 'jsqr'
+//import jsQR from 'jsqr'
+import QrCode from 'qrcode-reader'
 import QRious from 'qrious';
 
 function generateQR(): void {
@@ -19,6 +20,48 @@ function scanQR(): void {
 
   console.log("video: ", video);
 
+  var qr = new QrCode();
+
+
+
+  var n = <any>navigator;
+
+  n.getUserMedia = ( n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia  || n.msGetUserMedia );
+
+  n.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then(function(stream: MediaSource) {
+    console.log("Stream");
+    
+    video.srcObject = stream;
+    video.play();
+
+    console.log("outVideoCanvas: " + outVideoCanvas);
+    var context = outVideoCanvas.getContext('2d');
+
+    var interval = setInterval(function() {
+      if (context !== null) {
+        console.log("Rendering!");
+        outVideoCanvas.width = video.videoWidth;
+        outVideoCanvas.height = video.videoHeight;
+        context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+
+        let imageData: any = context.getImageData(0,0,video.videoWidth,video.videoHeight);
+        qr.decode(imageData);
+      }
+    }, 300);
+    
+    qr.callback = function(error: any, result: any) {
+      if(error) {
+        console.log(error)
+        return;
+      }
+      console.log(result)
+      clearInterval(interval);
+    }
+  });
+
+
+
+/*
   var n = <any>navigator;
 
   n.getUserMedia = ( n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia  || n.msGetUserMedia );
@@ -48,10 +91,9 @@ function scanQR(): void {
         }
       }
     }, 300);
-
-    /*
-    */
   });
+
+  */
 }
 
 // Expose the function to the browser
